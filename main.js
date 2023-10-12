b0806c5871b849adbed9c11954ff15c6c57d12ef
dwika10 <dwikajaya2904@gmail.com>
2023-10-12 15:01:28 +0700

    today

---
+++ main.js
@@ -0,0 +1,30 @@
+// console.log ("Hello world")
+// alert("Notifikasi")
+// prompt("dwika")
+
+// var Promnet = "coding is easy"
+// console.log(Promnet)
+// var Promnet = "coding not easy"
+// console.log(Promnet)
+
+// let Promnet = "coding is easy"
+// console.log(Promnet)
+// Promnet = "coding not easy"
+// console.log(Promnet)
+
+// const Promnet = "coding is easy"
+// console.log(Promnet)
+// Promnet = "coding not easy"
+// console.log(Promnet)
+
+
+
+// let p = document.querySelector("p")
+// let button = document.querySelector("button")
+// let input = document.querySelector("input")
+
+// button.addEventListener('click', function(){
+//     let isi = input.value
+//     console.log(isi)
+//     p.innerText =isi
+// })
