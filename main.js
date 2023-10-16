f5e5d216c7c8aa6e5a9d34aecadf0c723b9e2aba
dwika10 <dwikajaya2904@gmail.com>
2023-10-16 22:31:43 +0700

    fix

--- main.js
+++ main.js
@@ -19,12 +19,12 @@
 
 // let x = 6;
 // let y = 3;
+
 // console.log(x<10 && y>1)
 // console.log(x<10 && y < 1)
 // console.log(x==5||y==5)
 // console.log(x==6||y==5)
 // console.log(!(x==y))
-
 
 // let p = document.querySelector("p")
 // let button = document.querySelector("button")
@@ -33,5 +33,48 @@
 // button.addEventListener('click', function(){
 //     let isi = input.value
 //     console.log(isi)
-//     p.innerText =isi
+//     p.innerHTML = isi
+//     // p.innerText = isi
 // })
+
+let button = document.querySelector("#tombol_form");
+let input = document.querySelector("#input_form");
+let listContainer = document.querySelector("#list-container");
+let ul = document.querySelector("#list-container");
+
+ul.style.listStyleType = "none";
+
+button.addEventListener('click', function(){
+    let isi = input.value;
+    console.log(isi);
+
+    let listItem = document.createElement("li");
+    let checkbox = document.createElement("input");
+
+    checkbox.type = "checkbox";
+    let label = document.createElement("label");
+
+    label.textContent = " " + isi;
+    let deleteButton = document.createElement("button");
+    
+    deleteButton.textContent = " x";
+    checkbox.addEventListener('change', function() {
+        if (checkbox.checked) {
+            label.style.textDecoration = "line-through";
+        } else {
+            label.style.textDecoration = "none";
+        }
+    });
+
+    deleteButton.addEventListener('click', function() {
+        listItem.remove();
+    });
+
+    listItem.appendChild(checkbox);
+    listItem.appendChild(label);
+    listItem.appendChild(deleteButton);
+
+    listContainer.appendChild(listItem);
+
+    input.value = "";
+});
